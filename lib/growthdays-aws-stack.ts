2f62ec1ca840a471f0b1d5927619314c49f791dd
Jose Avalos <jose.b.avalos@gmail.com>
2022-04-21 14:11:52 -0500

    main - init push to S3

--- lib/growthdays-aws-stack.ts
+++ lib/growthdays-aws-stack.ts
@@ -1,5 +1,9 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {CfnOutput, Duration, RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {SubnetType, Vpc} from "aws-cdk-lib/aws-ec2";
+import {Bucket, BucketEncryption, HttpMethods, StorageClass} from "aws-cdk-lib/aws-s3";
+import {AccountRootPrincipal} from "aws-cdk-lib/aws-iam";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GrowthdaysAwsStack extends Stack {
@@ -7,10 +11,66 @@ export class GrowthdaysAwsStack extends Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // CHANGE: We have created the vpc object from the Vpc class.
+    const vpc = new Vpc(this, 'MainVpc',{
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GrowthdaysAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      // CHANGE: this is where we define how many AZs to use
+      maxAzs: 2,
+
+      // CHANGE: We define a single subnet configuration per AZ.
+      subnetConfiguration:  [
+        {
+          // CHANGE: this is it's CIDR mask so 255.255.255.0
+          cidrMask: 24,
+
+          // CHANGE: a name for each of these subnets
+          name: 'public-subnet',
+
+          // CHANGE: and the subnet type to be used - here we will have
+          // a public subnet. There are other options available here.
+          subnetType: SubnetType.PUBLIC
+        },
+      ]
+    });
+
+    const s3Bucket = new Bucket(this, 's3-bucket',{
+      // bucketName: 'growth-days-bucket', // not recommended for globally unique name
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+      publicReadAccess: false,
+      encryption: BucketEncryption.S3_MANAGED,
+      websiteIndexDocument: 'index.html',
+      cors: [
+        {
+          allowedMethods: [
+              HttpMethods.GET,
+              HttpMethods.POST,
+              HttpMethods.PUT,
+              HttpMethods.DELETE
+          ],
+          allowedOrigins: ['*'],
+          allowedHeaders:  ['*']
+        }
+      ],
+      lifecycleRules: [
+        {
+          abortIncompleteMultipartUploadAfter: Duration.days(90),
+          expiration: Duration.days(56),
+          transitions: [
+            {
+              storageClass: StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: Duration.days(30)
+            }
+          ]
+        }
+      ]
+    })
+
+    const cfnOutput = new CfnOutput(this, 'WebAppURL',{
+      value: `https://${s3Bucket.bucketDomainName}/index.html`,
+      description: 'The URL for out WebApp',
+      exportName: 'webAppURL'
+    })
   }
 }
