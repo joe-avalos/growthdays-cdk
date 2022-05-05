04e1852f2d62c9babbca3107891c849cefb3c8c6
Jose Avalos <jose.b.avalos@gmail.com>
2022-05-05 11:26:00 -0500

    main - seed undo

--- lib/growthdays-aws-stack.ts
+++ lib/growthdays-aws-stack.ts
@@ -11,6 +11,8 @@ import {Cluster, ContainerImage} from "aws-cdk-lib/aws-ecs";
 import {ApplicationLoadBalancedFargateService} from "aws-cdk-lib/aws-ecs-patterns";
 import {Repository} from "aws-cdk-lib/aws-ecr";
 import {Bucket, BucketEncryption, HttpMethods, StorageClass} from "aws-cdk-lib/aws-s3";
+import {Secret} from "aws-cdk-lib/aws-secretsmanager";
+import {StringParameter} from "aws-cdk-lib/aws-ssm";
 
 export class GrowthdaysAwsStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -32,9 +34,27 @@ export class GrowthdaysAwsStack extends Stack {
             ],
             enableDnsSupport: true,
         });
+
+        const dbSecret = new Secret(this, 'Secret', {
+            secretName: 'postgres-credentials',
+            generateSecretString: {
+                secretStringTemplate: JSON.stringify({
+                    username: 'postgres'
+                }),
+                excludePunctuation: true,
+                includeSpace: false,
+                generateStringKey: 'password',
+            }
+        })
+
+        new StringParameter(this, 'DBCredentialsArn', {
+            parameterName: 'db-credentials-arn',
+            stringValue: dbSecret.secretArn,
+        })
+
         const rdsInstance = new DatabaseInstance(this, 'Database', {
             engine: DatabaseInstanceEngine.postgres({version: PostgresEngineVersion.VER_13_3}),
-            credentials: Credentials.fromGeneratedSecret('postgres'),
+            credentials: Credentials.fromSecret(dbSecret),
             instanceType: InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO),
             vpcSubnets: {
                 subnetType: SubnetType.PRIVATE_ISOLATED,
@@ -51,6 +71,13 @@ export class GrowthdaysAwsStack extends Stack {
             deletionProtection: false,
             databaseName: 'growthDays',
             publiclyAccessible: false,
+        })
+
+        const dbEndpointOutput = new CfnOutput(this, 'dbEndpoint', {
+            value: rdsInstance.instanceEndpoint.hostname
+        })
+        const secretNameOutput = new CfnOutput(this, 'secretName', {
+            value: <string>rdsInstance.secret?.secretName
         })
 
         const ecsCluster = new Cluster(this, 'MyCluster', {
@@ -72,14 +99,6 @@ export class GrowthdaysAwsStack extends Stack {
             jsonField: 'username'
         })
 
-        const dbName = SecretValue.secretsManager(<string>rdsInstance.secret?.secretName, {
-            jsonField: 'dbname'
-        })
-
-        const dbPort = SecretValue.secretsManager(<string>rdsInstance.secret?.secretName, {
-            jsonField: 'port'
-        })
-
         const dbPass = SecretValue.secretsManager(<string>rdsInstance.secret?.secretName, {
             jsonField: 'password'
         })
@@ -92,10 +111,10 @@ export class GrowthdaysAwsStack extends Stack {
                 image,
                 environment: {
                     DB_HOST: rdsInstance.instanceEndpoint.hostname.toString(),
-                    DB_NAME: <string><unknown>dbName,
+                    DB_NAME: 'growthDays',
                     DB_USER: <string><unknown>dbUser,
                     DB_PASSWORD: <string><unknown>dbPass,
-                    DB_PORT: <string><unknown>dbPort
+                    DB_PORT: '5432'
                 }
             },
             assignPublicIp: true,
@@ -141,13 +160,6 @@ export class GrowthdaysAwsStack extends Stack {
           value: `https://${s3Bucket.bucketDomainName}/index.html`,
           description: 'The URL for out WebApp',
           exportName: 'webAppURL'
-        })
-
-        const dbEndpointOutput = new CfnOutput(this, 'dbEndpoint', {
-            value: rdsInstance.instanceEndpoint.hostname
-        })
-        const secretNameOutput = new CfnOutput(this, 'secretName', {
-            value: <string>rdsInstance.secret?.secretName
         })
     }
 }
